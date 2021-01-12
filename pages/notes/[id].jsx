429d050a68372d5d9f7401f5aefc42903900662e
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-01-12 17:19:18 -0300

    define env variables

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -17,7 +17,7 @@ const Note = () => {
 export default Note;
 
 export async function getServerSideProps({ params, req, res }) {
-  const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
+  const response = await fetch(`${API_URL}/api/note/${params.id}`);
 
   if (!response.ok) {
     res.writeHead(302, { Location: "/notes" });
