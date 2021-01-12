0642e627da568546d9af46889d129302766cdb05
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2021-01-11 21:26:05 -0300

    add links to app

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
-const Page = () => <div>Index Page</div>
+const Page = () => (
+  <div>
+    <h1>Index Page</h1>
+    <Link href="/notes">Notes Page</Link>
+  </div>
+);
 
 export default Page;
